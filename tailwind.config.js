1cb69007073bf5519793c90f257e58cc4b767ae9
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-05 17:31:32 -0300

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif']
+      },
+      colors: {
+        'red-brand': '#DB2F27',
+        'red-light': '#ff4633'
+      }
+    }
+  },
+  plugins: []
+}
