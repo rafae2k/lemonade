457160ebc541a976f71ef91f91e8b5f8667aa638
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-06 19:20:13 -0300

    feat: add `RecoilRoot`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,20 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { SessionProvider } from 'next-auth/react'
 
 import 'styles/global.css'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <SessionProvider session={session}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </QueryClientProvider>
-    </SessionProvider>
+    <RecoilRoot>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+        </QueryClientProvider>
+      </SessionProvider>
+    </RecoilRoot>
   )
 }
 
