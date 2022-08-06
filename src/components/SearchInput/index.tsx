ce99fc72f60335537d579453b9b1aa5b79b8d2a0
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-06 15:04:26 -0300

    refactor: add data loader

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,31 +1,31 @@
 import { IoSearchOutline as SearchIcon } from 'react-icons/io5'
 import { BsArrowRightShort as ArrowRightIcon } from 'react-icons/bs'
-import { FiPlus as PlusIcon } from 'react-icons/fi'
+import { v4 as uuid } from 'uuid'
 
 import { useState } from 'react'
 import SearchItem from 'components/SearchItem'
-import { useArtistAutocomplete } from 'services/api/setlist'
+import { useArtistAutocomplete } from 'services/api/getArtists'
+import SearchContainer from 'components/SearchContainer'
 
 const SearchInput = () => {
   const [input, setInput] = useState('')
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value)
   }
 
-  // const { data } = useArtistAutocomplete(input)
-
-  // console.log(data)
+  const { data, isLoading } = useArtistAutocomplete(input)
 
   return (
     <div className="flex flex-col items-center gap-4">
       {/* SEARCH RESULTS CARD */}
-      {/* <div className="flex flex-col w-full max-w-4xl gap-2 p-2 overflow-y-scroll bg-white rounded-3xl sm:p-8 max-h-[calc(100vh-10rem)] sm:gap-4"> */}
-      {/* SEARCH ITEM */}
-      {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((_, i) => (
-          <SearchItem key={i} />
-        ))} */}
-      {/* </div> */}
+
+      <SearchContainer isLoading={isLoading}>
+        {data !== undefined &&
+          data.items.map((artist) => (
+            <SearchItem type="artist" artist={artist} key={uuid()} />
+          ))}
+      </SearchContainer>
 
       {/* SEARCH INPUT */}
       <div className="flex items-center justify-between w-full max-w-6xl gap-2 px-4 transition-colors rounded-full bg-red-light focus-within:bg-black">
