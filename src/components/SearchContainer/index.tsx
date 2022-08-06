68b41a8d37e0dd8a0f5a8d91458d4f6d9c132e77
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-06 19:19:29 -0300

    refactor: add loading and container id

--- src/components/SearchContainer/index.tsx
+++ src/components/SearchContainer/index.tsx
@@ -1,4 +1,5 @@
 import { Bars, useLoading } from '@agney/react-loading'
+import { useEffect, useRef } from 'react'
 
 type SearchContainerProps = {
   children: React.ReactNode
@@ -6,16 +7,16 @@ type SearchContainerProps = {
 }
 
 const SearchContainer = ({ children, isLoading }: SearchContainerProps) => {
-  const { indicatorEl } = useLoading({
+  const { containerProps, indicatorEl } = useLoading({
     loading: true,
     indicator: <Bars width="50" />
   })
 
   return (
     <div
-      className={`flex flex-col w-full max-w-4xl gap-2 p-2 overflow-y-scroll bg-white rounded-3xl sm:p-8 max-h-[calc(100vh-10rem)] sm:gap-4 first-letter:
-        ${isLoading && !children ? 'hidden' : ''}
-      `}
+      id="search-container"
+      className={`flex flex-col w-full max-w-4xl gap-2 p-2 overflow-y-scroll bg-white rounded-3xl sm:p-8 max-h-[calc(100vh-10rem)] sm:gap-4
+      ${!children ? 'hidden' : ''} ${isLoading ? 'justify-center' : ''}`}
     >
       {isLoading ? indicatorEl : children}
     </div>
