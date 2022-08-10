90549d2b71116b98a84051407f06fd64fae05f98
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-09 21:23:01 -0300

    feat: add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import type { AppProps } from 'next/app'
-import { useState } from 'react'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { SessionProvider } from 'next-auth/react'
-
 import 'styles/global.css'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
 import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
+
+  const HJID = 3100354
+  const HJSV = 6
+
+  useEffect(() => {
+    hotjar.initialize(HJID, HJSV)
+  }, [])
 
   return (
     <RecoilRoot>
