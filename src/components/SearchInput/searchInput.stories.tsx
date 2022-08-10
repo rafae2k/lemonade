33ccd2fa2a38632289f7e9e8873f811c65f254f0
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-09 21:30:32 -0300

    refactor: move unrelated logic and components

--- src/components/SearchInput/searchInput.stories.tsx
+++ src/components/SearchInput/searchInput.stories.tsx
@@ -1,4 +1,5 @@
-import { Story, Meta } from '@storybook/react'
+import { Meta, Story } from '@storybook/react'
+
 import SearchInput from '.'
 export default {
   title: 'SearchInput',
--- src/components/SearchInput/searchInput.test.tsx
+++ src/components/SearchInput/searchInput.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+
 import SearchInput from '.'
 
 describe('<SearchInput />', () => {
