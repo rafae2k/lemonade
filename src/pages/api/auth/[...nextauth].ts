56c30cf8a5fa9789325805826cf0adb5e8141945
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-09 21:22:11 -0300

    refactor: refresh token

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,23 @@
 import spotifyApi, { LOGIN_URL } from 'lib/spotify'
 import { NextApiRequest, NextApiResponse } from 'next'
-import NextAuth, { Account, NextAuthOptions } from 'next-auth'
+import NextAuth, { Account, NextAuthOptions, User } from 'next-auth'
 import SpotifyProvider from 'next-auth/providers/spotify'
 
 const refreshAccessToken = async (token: Account) => {
   try {
     // @ts-ignore
-    spotifyApi.setAccessToken(token.access_token)
-    // @ts-ignore
-    spotifyApi.setRefreshToken(token.refresh_token as string)
+    spotifyApi.setRefreshToken(token.refreshToken as string)
 
     const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+
+    // @ts-ignore
+    spotifyApi.setAccessToken(refreshedToken.access_token)
 
     return {
       ...token,
       accessToken: refreshedToken.access_token,
       accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
-      refreshToken: refreshedToken.refresh_token ?? token.refresh_token
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken
     }
   } catch (error) {
     console.error(error)
@@ -37,23 +38,31 @@ const options: NextAuthOptions = {
   ],
   secret: process.env.JWT_SECRET as string,
   callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        spotifyApi.setAccessToken(account.access_token as string)
+    async jwt(token) {
+      if (token.account && token.user) {
+        spotifyApi.setAccessToken(token.account.access_token as string)
+
+        const expiresAt = token.account.expires_at * 1000
 
         return {
-          accessToken: account.access_token,
-          accessTokenExpires: Date.now() + account.expires_at * 1000,
-          refreshToken: account.refresh_token,
-          user
+          accessToken: token.account.access_token,
+          accessTokenExpires: token.account.expires_at,
+          refreshToken: token.account.refresh_token,
+          user: {
+            ...token.user,
+            profile: token.profile
+          }
         }
       }
 
-      if (Date.now() < token.accessTokenExpires) {
+      if (
+        token.token.expires_at === null ||
+        Date.now() < token.token.expires_at
+      ) {
         return token
       }
 
-      return refreshAccessToken(account)
+      return await refreshAccessToken(token.token)
     },
 
     async session({ session, token }) {
@@ -70,3 +79,35 @@ const SpotifyAuth = (req: NextApiRequest, res: NextApiResponse) =>
   NextAuth(req, res, options)
 
 export default SpotifyAuth
+
+type AuthToken = {
+  accessToken: string
+  accessTokenExpires: number | null
+  refreshToken: string | null
+  user: {
+    id: string
+    name: string
+    email: string
+    image: string
+  }
+  iat: number
+  exp: number
+  jti: string
+}
+
+type AccountAuth = {
+  provider: 'spotify'
+  type: 'oauth'
+  providerAccountId: string
+  access_token: string
+  token_type: 'Bearer'
+  expires_at: number
+  refresh_token: string
+  scope: string
+}
+
+type JWTResponse = {
+  token: AuthToken
+  user: User
+  account: AccountAuth
+}
