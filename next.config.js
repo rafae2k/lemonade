b5be4a6de55d261ce39104ffe48717eec143b719
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-09 21:26:54 -0300

    chore: ignore ts build errors and add cdn domains

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true
-  },
   images: {
-    domains: ['scontent-iad3-1.xx.fbcdn.net', 'i.scdn.co', 'www.whosampled.com']
+    domains: [
+      'scontent-iad3-1.xx.fbcdn.net',
+      'scontent-iad3-2.xx.fbcdn.net',
+      'i.scdn.co',
+      'www.whosampled.com'
+    ]
   }
 }
 
