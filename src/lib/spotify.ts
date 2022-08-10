f7189ff9f89336982f2218d3f05eac9bf6adaaa6
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-09 21:21:12 -0300

    refactor: remove unused permissions

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -2,24 +2,15 @@ import SpotifyWebApi from 'spotify-web-api-node'
 
 const scopes = [
   'ugc-image-upload',
-  'user-modify-playback-state',
-  'user-read-playback-state',
-  'user-read-currently-playing',
   'user-follow-modify',
-  'user-follow-read',
-  'user-read-recently-played',
-  'user-read-playback-position',
-  'user-top-read',
   'playlist-read-collaborative',
   'playlist-modify-public',
   'playlist-read-private',
   'playlist-modify-private',
-  'app-remote-control',
-  'streaming',
   'user-read-email',
   'user-read-private',
-  'user-library-modify',
-  'user-library-read'
+  'user-follow-modify',
+  'user-follow-read'
 ].join(',')
 
 const params = {
