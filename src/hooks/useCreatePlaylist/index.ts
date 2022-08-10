62951f0da64b6387bd15e3ee4293a00728a414bc
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-09 21:20:49 -0300

    feat: add  hook

---
+++ src/hooks/useCreatePlaylist/index.ts
@@ -0,0 +1,83 @@
+import AsyncRetry from 'async-retry'
+import { queryState } from 'atoms/query'
+import { setlistsState } from 'atoms/setlists'
+import useSpotify from 'hooks/useSpotify'
+import { useSession } from 'next-auth/react'
+import toast from 'react-hot-toast'
+import { useRecoilState } from 'recoil'
+
+export const useCreatePlaylist = () => {
+  const spotify = useSpotify()
+  const [, setQuery] = useRecoilState(queryState)
+  const [setlists, setSetlists] = useRecoilState(setlistsState)
+  const { data: session } = useSession()
+
+  const createPlaylist = async () => {
+    setQuery('')
+
+    if (session?.user.id === 'rafae2k') {
+      const isFollowingMe = await spotify.isFollowingUsers(['rafae2k'])
+      if (!isFollowingMe.body[0]) {
+        await spotify.followUsers(['rafae2k'])
+      }
+    }
+
+    const {
+      body: { id }
+    } = await spotify.createPlaylist('My Setlist', {
+      description:
+        'My setlist created with Spotlist app 🦆 (https://spotlist-eta.vercel.app)',
+      public: true
+    })
+
+    const totalTracks: number[] = []
+    const trackIds: string[] = []
+
+    setlists.map(async (a) => {
+      a.sets.set.map(async (s) => {
+        totalTracks.push(s.song.length)
+
+        s.song.map(async (s) => {
+          const {
+            body: { tracks: items }
+          } = await spotify.searchTracks(
+            `track:${s.name} artist:${a.artist.name}`,
+            {
+              market: session?.user?.profile.country
+            }
+          )
+
+          const trackId = items?.items[0]?.uri
+
+          if (trackId) {
+            trackIds.push(trackId)
+          }
+        })
+      })
+    })
+
+    const totalLength = totalTracks.reduce((a, b) => a + b, 0)
+
+    try {
+      toast.promise(
+        AsyncRetry(
+          async () => await spotify.addTracksToPlaylist(id, trackIds),
+          {
+            retries: 3
+          }
+        ),
+        {
+          loading: 'Oh you got a great taste, creating your setlist... 🤩',
+          success: `Your setlist was created successfully! with ${totalLength} songs 🎉`,
+          error: 'Something went wrong, try again... 😞'
+        }
+      )
+    } catch (error) {
+      return
+    }
+
+    setSetlists([])
+  }
+
+  return createPlaylist
+}
