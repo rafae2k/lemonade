58721e17038affbf76dba0cb785dd2861a90d2b5
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-11 18:35:23 -0300

    feat: add

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,10 +1,15 @@
+import { Transition } from '@headlessui/react'
+import { modalState } from 'atoms/modal'
 import { queryState } from 'atoms/query'
+import { setlistsState } from 'atoms/setlists'
 import { BsArrowRightShort as ArrowRightIcon } from 'react-icons/bs'
 import { IoSearchOutline as SearchIcon } from 'react-icons/io5'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 
 const SearchInput = () => {
   const [query, setQuery] = useRecoilState(queryState)
+  const [, setIsModalOpen] = useRecoilState(modalState)
+  const setlists = useRecoilValue(setlistsState)
 
   return (
     <div className="flex items-center justify-between w-full max-w-6xl gap-2 px-4 transition-colors rounded-full bg-red-light focus-within:bg-black">
@@ -18,12 +23,22 @@ const SearchInput = () => {
         onChange={(e) => setQuery(e.target.value)}
       />
 
-      <a
-        href="#"
-        className="p-2 transition-all bg-white border border-white rounded-full hover:bg-transparent hover:border-white"
+      <Transition
+        show={setlists.length > 0}
+        enter="transition-all duration-200 ease-in"
+        enterFrom="opacity-0 scale-90 translate-y-4"
+        enterTo="opacity-100 scale-100"
+        leave="transition-opacity duration-200"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-90 translate-y-4"
       >
-        <ArrowRightIcon className="text-4xl text-black hover:text-white " />
-      </a>
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="p-2 transition-all bg-white border border-white rounded-full hover:bg-transparent hover:border-white"
+        >
+          <ArrowRightIcon className="text-4xl text-black hover:text-white " />
+        </button>
+      </Transition>
     </div>
   )
 }
