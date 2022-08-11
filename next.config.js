d96e830a2789684dcf4d998cc99df14492c84ed1
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-11 18:04:10 -0300

    chore: add remotePatterns of image domains

--- next.config.js
+++ next.config.js
@@ -5,12 +5,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [
-      'scontent-iad3-1.xx.fbcdn.net',
-      'scontent-iad3-2.xx.fbcdn.net',
-      'i.scdn.co',
-      'www.whosampled.com'
-    ]
+    domains: ['i.scdn.co', 'www.whosampled.com']
+  },
+  experimental: {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'scontent-*.xx.fbcdn.net'
+        }
+      ]
+    }
   }
 }
 
