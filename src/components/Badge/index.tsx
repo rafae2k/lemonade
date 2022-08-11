2251763e48f6e1ac4731e37982c8c5d177b702b0
Rafael Feitoza <rafaeljr@outlook.com>
2022-08-11 18:33:00 -0300

    feat: add

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,4 +1,6 @@
 import { setlistsState } from 'atoms/setlists'
+// import Animation from 'components/Animation'
+// import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { Setlist } from 'services/api/getSetlists'
 
@@ -8,14 +10,25 @@ type BadgeProps = {
 
 const Badge = ({ set }: BadgeProps) => {
   const [setlists, setSetlists] = useRecoilState(setlistsState)
+  // const [show, setShow] = useState(false)
+
+  // useEffect(() => {
+  //   setShow(true)
+
+  //   return () => {
+  //     setShow(false)
+  //   }
+  // }, [])
 
   return (
+    // <Animation show={show}>
     <button
       className="px-2 py-1 h-8 bg-white rounded-full max-w-[10rem] shrink-0 overflow-hidden whitespace-nowrap text-ellipsis hover:opacity-80 duration-300 transition-opacity "
       onClick={() => setSetlists(setlists.filter((s) => s.id !== set.id))}
     >{`${set?.artist?.name} ${
       set?.tour?.name ? `- ${set?.tour?.name}` : ''
     }`}</button>
+    // </Animation>
   )
 }
 
